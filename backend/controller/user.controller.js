1376fef34511d85c3b784b88a018b9c66395e7ce
1234Ganesh <ganeshkonda346@gmail.com>
2025-02-05 12:36:07 +0530

    updated url

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -102,8 +102,8 @@ exports.login = async (req, res) => {
       .status(200)
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
-        httpsOnly: true,
-        sameSite: "strict",
+        httpsOnly: false,
+        sameSite: "none",
       })
       .json({
         message: `Welcome back ${user.name}`,
