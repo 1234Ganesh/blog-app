6beb80ec76ea829f034a4e869b26f5b1a221bc16
1234Ganesh <138300395+1234Ganesh@users.noreply.github.com>
2025-02-05 14:18:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,15 @@ app.use(
   })
 );
 
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(200);
+});
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
