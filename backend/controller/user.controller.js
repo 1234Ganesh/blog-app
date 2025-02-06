a7ca3dd6cb76cdd9c5704044a025693b00723f29
1234Ganesh <138300395+1234Ganesh@users.noreply.github.com>
2025-02-06 11:06:47 +0530

    Update user.controller.js

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -118,15 +118,10 @@ exports.login = async (req, res) => {
 
 exports.logout = (req, res) => {
   try {
-    return res
-      .status(200)
-      .cookie("token", "", {
-        maxAge: 0,
-      })
-      .json({
-        message: "Logged out Successfully",
-        success: true,
-      });
+     return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+      message: "Logged out successfully",
+      success: true,
+    });
   } catch (error) {
     console.log(error);
   }
